4e074a9f36ef5756ec150b80b3e681f18f3f381c
Nixxa <52290162+sogeking-pixel@users.noreply.github.com>
2025-03-30 21:18:51 -0500

    Ya se puede mandar un response 200 ok

--- src/main/java/Class/Response.java
+++ src/main/java/Class/Response.java
@@ -4,17 +4,62 @@
  */
 package Class;
 
+import java.util.Objects;
+
 /**
  *
  * @author yerso
  */
 public class Response extends HttpMessages {
-    private String start_line = null;
-    private String headers = null;
-    private String body = null;
-    private boolean emptyLine = false;
+    private StartLine starLine;
 
+    public Response(String protocol, int statusCode, String statusText) {
+        this.starLine = new StartLine(protocol, statusCode, statusText);
+    }
+
+    public StartLine getStarLine() {
+        return starLine;
+    }
+
+    public void setStarLine(StartLine starLine) {
+        this.starLine = starLine;
+    }
+
+    
+   
+    @Override
+   public String toString(){
+       return starLine + "\r\n";
+   }
+   
+   class StartLine{
+       private String protocol = "";
+       private int statusCode;
+       private String statusText = "";
+
+        public StartLine(String protocol, int statusCode, String statusText) {
+            this.protocol = protocol;
+            this.statusCode = statusCode;
+            this.statusText = statusText;
+        }
+       
+       @Override
+        public String toString(){
+            return protocol + " " + statusCode + " " + statusText + "\r\n";
+        }       
+   }
+   
+   class Header{
+   
+   }
+   
+   class Body{
+       
+   }
+   
     
     
     
 }
+
+
--- src/main/java/Server/ServerHttp.java
+++ src/main/java/Server/ServerHttp.java
@@ -6,9 +6,14 @@ package Server;
 
 import Class.Response;
 import Class.Resquest;
+import java.io.BufferedReader;
 import java.io.IOException;
-//import java.net.ServerSocket;
-//import java.net.Socket;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 /**
  *
  * @author yerso
@@ -21,33 +26,51 @@ public class ServerHttp {
             Los separadores del formato sera esto:
             https://mdn.github.io/shared-assets/images/diagrams/http/messages/http-message-anatomy.svg
         */
-//        int port = 9999;
+        int port = 9999;
         
-        String request_text = "POST /users HTTP/1.1\r\n" +
-                              "Host: example.com\r\n" +
-                              "Content-Type: application/x-www-form-urlencoded\r\n" +
-                              "Content-Length: 5\r\n"+
-                              "\r\n" +
-                              "hola mundo, este es el maldito body xddxdx";
-        
-        System.out.println("First Line: " + Resquest.GetStartLine(request_text));
-        System.out.println("Headers: " + Resquest.GetHeader(request_text));
-        System.out.println("Body: " + Resquest.GetBody(request_text));
-
-
-        
-//        try (ServerSocket serverSocket = new ServerSocket(port)) {
-//            while(true){
-//                
-//                System.out.println("Wait client for port: " + port + "...");
-//            
-//                Socket socket = serverSocket.accept();
-//                System.out.println("Cliente Conected: " + socket.getInetAddress());
-//            }
+//        String request_text = "POST /users HTTP/1.1\r\n" +
+//                              "Host: example.com\r\n" +
+//                              "Content-Type: application/x-www-form-urlencoded\r\n" +
+//                              "Content-Length: 5\r\n"+
+//                              "\r\n" +
+//                              "hola mundo, este es el maldito body xddxdx";
+//        
+//        System.out.println("First Line: " + Resquest.GetStartLine(request_text));
+//        System.out.println("Headers: " + Resquest.GetHeader(request_text));
+//        System.out.println("Body: " + Resquest.GetBody(request_text));
 //
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        Response response = new Response("HTTP/1.1", 200, "OKI DOKI");
+        
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            
+           while(true){     
+                System.out.println("Wait client for port: " + port + "...");
+
+                Socket socket = serverSocket.accept();
+
+                System.out.println("Cliente Conected: " + socket.getInetAddress());                
+                
+                InputStream input = socket.getInputStream();
+                OutputStream output = socket.getOutputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                String mensajeRecibido = reader.readLine();
+                System.out.println("Cliente dice: " + mensajeRecibido);
+
+                // Respuesta del servidor
+                String respuesta =  response.toString();
+                byte[] respuestaBytes = respuesta.getBytes(); // Convertir String a bytes
+                output.write(respuestaBytes); // Enviar respuesta
+
+                // Cerrar conexión
+                socket.close();
+                System.out.println("Conexion cerrada.");
+                
+           }
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
  
